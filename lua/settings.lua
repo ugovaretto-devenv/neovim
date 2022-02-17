0ce8df16c8d748450957c3c391a73b69562d453a
Ugo Varetto <ugovaretto@gmail.com>
2022-02-17 17:03:06 +0800

    created

---
+++ lua/settings.lua
@@ -0,0 +1,11 @@
+vim.cmd [[
+set autoindent
+set expandtab
+set shiftwidth=4
+set smartindent
+set softtabstop=4
+set tabstop=4
+set number
+set colorcolumn=80
+highlight ColorColumn ctermbg=0 guibg=lightgrey
+]]
